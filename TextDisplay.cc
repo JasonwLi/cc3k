ca276e337284c0a436ee6c363ff67b1a06c84d36
Jason <z467li@edu.uwaterloo.ca>
2017-07-21 20:31:40 -0400

    Update TextDisplay.cc

---
+++ TextDisplay.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "TextDisplay.h"
+
+using namespace std;
+
+void TextDisplay::notify(int length, int width, char ch){
+    theDisplay[length][width] = ch;
+}
+
+void TextDisplay::print(ostream &out){
+    int i;
+    int j;
+    for (i=0; i < length; i++){
+        for(j=0; j < width; j++){
+            out << theDisplay[i][j];
+        out << endl;
+    return out;
+}
+
+TextDisplay::TextDisplay(int length, int width):length(length), width(width){
+    for (int i = 0; i < length; i++){
+        vector<char> line;
+        for (int k = 0; k < width; k++){
+            line.push_back(' ');
+        }
+        theDisplay.push_back(line);
+    }
+}
+
+TextDisplay::~TextDisplay(){}
