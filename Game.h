d66b0f7261494130aa6ce3a80ef7435d0caec98a
Jason <z467li@edu.uwaterloo.ca>
2017-07-23 18:48:11 -0400

    Update Game.h

---
+++ Game.h
@@ -0,0 +1,51 @@
+#ifndef _GAME_H
+#define _GAME_H
+#include <string>
+#include <vector>
+
+class Tile;
+class Player;
+class Enemy;
+class Component;
+class Controller;
+
+using namespace std;
+
+class Game{
+    private:
+        Tile** theFloor;
+        int row;
+        int column;
+        Player *pc;
+        
+        int floorLevel;
+        vector<Enemy*> enemies;
+        Controller* control;
+    public:
+        Game(int row, int column);
+        ~Game();
+        void build(Controller* ctrl);
+        void loadFloor(string floorTemp);
+        void createFloor();
+        void createPC();
+        void createStair();
+        void createEnemies();
+        void createPotions();
+        void createTreasure();
+        void createFloor();
+        void createChambers();
+        int getRow();
+        int getCol();
+        int getLevel();
+        Tile* getRandTile();
+        Tile* getTile(int row, int col);
+        Player* getPC();
+        Enemy** getEnemies();
+        void clearFloor();
+        void setLevel(int num);
+        void setPC(Player *p);
+        void notify(int row, int col, char ch);
+        string attack(string dir);
+        string usePotion(string dir);
+};        
+#endif
