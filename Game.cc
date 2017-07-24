48dbdc748ea4b13bf476d10062b2c3ada4132903
Jason <z467li@edu.uwaterloo.ca>
2017-07-24 13:01:29 -0400

    Update Game.cc

--- Game.cc
+++ Game.cc
@@ -2,6 +2,7 @@
 #include "Tile.h"
 #include "Controller.h"
 #include "Component.h"
+#include "DragonHoards.h"
 #include "Stair.h"
 #include "Player.h"
 #include "Enemy.h"
@@ -300,6 +301,44 @@ void Game::loadFloor(string floorTemp){
                 theFloor[j][k].setComponent(s);
                 s->setLocation(&(theFloor[j][k]));
             }
+            else if(c == 'H'){
+                Human* h = new Human();
+                enemies.push_back(h);
+                theFloor[j][j].setComponent(h);
+                h->setLocation(&(theFloor[j][k]));
+            }
+            else if(c == 'O'){
+                Orc* o = new Orc();
+                enemies.push_back(o);
+                theFloor[j][j].setComponent(o);
+                o->setLocation(&(theFloor[j][k]));
+            }
+            else if(c == 'L'){
+                Halfling* l = new Halfling();
+                enemies.push_back(l)
+                theFloor[j][k].setComponent(l);
+                l->setLocation(&(theFloor[j][k]));
+            }
+            else if(c == 'M'){
+                Merchant* m = new Merchant();
+                enemies.push_back(m)
+                theFloor[j][k].setComponent(m);
+                m->setLocation(&(theFloor[j][k]));
+
+            }
+            else if(c == 'E'){
+                Elf* e = new Elf();
+                enemies.push_back(e)
+                theFloor[j][k].setComponent(e);
+                e->setLocation(&(theFloor[j][k]));
+
+            }
+            else if(c == 'W'){
+                Dwarf* w = new Dwarf();
+                enemies.push_back(w)
+                theFloor[j][k].setComponent(w);
+                w->setLocation(&(theFloor[j][k]));
+            }
             else if(c == '0'){
                 Potion* p = new Potion("BD");
                 potions.push_back(p);
@@ -345,44 +384,6 @@ void Game::loadFloor(string floorTemp){
                 theFloor[i][j].setObject(h);
                 h->setPosition(&(theFloor[i][j]));
             }
-            else if(c == 'H'){
-                Human* h = new Human();
-                enemies.push_back(h);
-                theFloor[j][j].setComponent(h);
-                h->setLocation(&(theFloor[j][k]));
-            }
-            else if(c == 'L'){
-                Halfling* l = new Halfling();
-                enemies.push_back(l)
-                theFloor[j][k].setComponent(l);
-                l->setLocation(&(theFloor[j][k]));
-            }
-            else if(c == 'O'){
-                Orc* o = new Orc();
-                enemies.push_back(o);
-                theFloor[j][j].setComponent(o);
-                o->setLocation(&(theFloor[j][k]));
-            }
-            else if(c == 'M'){
-                Merchant* m = new Merchant();
-                enemies.push_back(m)
-                theFloor[j][k].setComponent(m);
-                m->setLocation(&(theFloor[j][k]));
-
-            }
-            else if(c == 'W'){
-                Dwarf* w = new Dwarf();
-                enemies.push_back(w)
-                theFloor[j][k].setComponent(w);
-                w->setLocation(&(theFloor[j][k]));
-            }
-            else if(c == 'E'){
-                Elf* e = new Elf();
-                enemies.push_back(e)
-                theFloor[j][k].setComponent(e);
-                e->setLocation(&(theFloor[j][k]));
-
-            }
             else if(c == '7'){
                 SmallHoard* sh = new SmallHoard();
                 treasures.push_back(sh);
@@ -427,8 +428,146 @@ void Game::loadFloor(string floorTemp){
 }
 
 string Game::Attack(string dir){
+    int dnum;
+    if(dir == "no"){
+        dnum = 0;
+    }else if(dir == "we"){
+        dnum = 1;
+    }else if(dir == "so"){  
+        dnum = 2;
+    else if(dir == "ea"){  
+        dnum = 3;
+    }else if(dir == "nw"){  
+        dnum = 4;
+    }else if(dir == "ne"){  
+        dnum = 5;
+    }else if(dir == "sw"){  
+        dnum = 6;
+    }else if(dir == "se"){  
+        dnum = 7;
+    }
+    Tile* dest = pc->getLocation()->getNeighbour(dnum);
+    Component* comp = dest->getComponent();
     
+    string outmsg = "";
+    if(comp && comp->getType() == "enemy"){
+        Enemy* monster = static_cast<Enemy*>(comp);
+        outmsg += pc->hit(monster);
+        // if dragon is killed delete it from its hoard.
+        if(monster->getName() == "Dragon" && monster->getHP() < 1){
+            static_cast<Dragon*>(monster)->getHoard()->slayDragon();
+        }
+    }
+    else{
+        outmsg += "There is no enemy in that directioN!";
+    }
+    return message;
 }
 
 string Game::usePotion(string dir){
+    int dnum;
+    if(dir == "no"){
+        dnum = 0;
+    }else if(dir == "we"){
+        dnum = 1;
+    }else if(dir == "so"){
+        dnum = 2;
+    else if(dir == "ea"){
+        dnum = 3;
+    }else if(dir == "nw"){
+        dnum = 4;
+    }else if(dir == "ne"){
+        dnum = 5;
+    }else if(dir == "sw"){
+        dnum = 6;
+    }else if(dir == "se"){ 
+        dnum = 7;
+    }
+    Tile* dest = pc->getLocation()->getNeighbour(dest);
+    getLocation comp = dest->getComponent();
+
+    string outmsg = "";
+
+    if(!comp || comp->getType() != "potionion"){
+        return "No potionion exists on the" + dir +" tile.";
+    }
+    else{
+        potionion* p = static_cast<potionion*>(comp);
+
+        if(p->getEffect() == "RH"){
+            int num = 10;
+            if(pc->getRace() == "Drow"){
+                    num = num*1.5;
+            }
+            pc->heal(num);
+            outmsg += "used a health potion from " + dir +" healed " + intToString(n) + " Health. ";
+
+        }
+        else if(p->getEffect() == "PH"){
+            int num = 10;
+            if(pc->getRace() == "Drow"){
+                    num = num*1.5;
+            }
+            pc->takeHit(num);
+            outmsg += "used a poison potion from " + dir +" lost " + intToString(num) + " Health. ";
+        }
+        else if(p->getEffect() == "BA"){
+            int num = 5;
+            if(pc->getRace() == "Drow"){
+                    num = num*1.5;
+            }
+            pc = new boostAtk(num, pc);
+            outmsg += "used a damage boost potion from " + dir +"  gain " + intToString(num) + " Attack power. ";
+        }
+        else if(p->getEffect() == "WA"){
+            int num = 5;
+            if(pc->getRace() == "Drow"){
+                    num = num*1.5;
+            }           
+            pc = new boostAtk(-num, pc);
+            outmsg += "used a damage wound potion from " + dir +" lost " + intToString(num) + " Attack power. ";
+        }
+        else if(p->getEffect() == "BD"){
+            int num = 5;
+            if(pc->getRace() == "Drow"){
+                    num = num*1.5;
+            }           
+            pc = new boostDef(num, pc);
+            outmsg += "used a defence boost potion from " + dir +" gain " + intToString(num) + " Defense power ";
+        }
+        else if(p->getEffect() == "WD"){
+            int num = 5;
+            if(pc->getRace() == "Drow"){
+                    num = num*1.5;
+            }           
+            pc = new boostDef(-num, pc);
+            outmsg += "used a defence wound potion from " + dir +" lost " + intToString(num) + " Defense power";
+        }
+        p->getLocation()->setComponent(nullptr);
+        delete p;
+        return outmsg;
+    }
+}
+
+int getChamber(int row, int col){
+    if(col >= 3 && col <= 28 && row >= 3 && row <= 6){
+        return 1;
+    }
+    if((col >= 39 && col <= 61 && row >= 3 && row <= 6)  ||
+       (col >= 61 && col <= 75 && row >= 7 && row <= 13) ||
+       (col >= 62 && col <= 69 && row == 5)              ||
+       (col >= 62 && col <= 72 && row == 6)){
+        return 2;
+    }
+    if(col >= 38 && col <= 50 && row >= 10 && row <= 12){
+        return 3;
+    }
+    if(col >= 4 && col <= 24 && row >= 15 && row <= 21){
+        return 4;
+    }
+    if((col >= 37 && col <= 75 && row >= 19 && row <= 21) ||
+       (col >= 65 && col <= 75 && row >= 16 && row <= 18)){
+        return 5;
+    }
+    return 0;
 }
